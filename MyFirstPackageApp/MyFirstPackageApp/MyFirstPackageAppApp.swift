3aca04b8deeb2e861c40b81f03a40680b4c48f5c
Pedro Maria Moreno Gonzalez <pmorenog@emeal.nttdata.com>
2024-05-20 08:35:30 +0200

    [FEATURE] Project creation

---
+++ MyFirstPackageApp/MyFirstPackageApp/MyFirstPackageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstPackageAppApp.swift
+//  MyFirstPackageApp
+//
+//  Created by Pedro Maria Moreno Gonzalez on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstPackageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyFirstPackageApp/MyFirstPackageAppUITests/MyFirstPackageAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyFirstPackageAppUITestsLaunchTests.swift
+//  MyFirstPackageAppUITests
+//
+//  Created by Pedro Maria Moreno Gonzalez on 20/5/24.
+//
+
+import XCTest
+
+final class MyFirstPackageAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
