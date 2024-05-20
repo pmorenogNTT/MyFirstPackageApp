dd43ebdb5bb07a6aaca1e814149c13270cc15a9b
Daniel Ayala <dayalaja@nttdata.com>
2024-05-20 11:44:07 +0200

    FEATURE:  Theme package

---
+++ MyFirstPackageApp/MyFirstPackageApp/ThemePackage/Package.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ThemePackage",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ThemeColorsLibrary",
+            targets: ["ThemeColors"]
+        ),
+        .library(
+            name: "ThemeIconsLibrary",
+            targets: ["ThemeIcons"]
+        ),
+        .library(
+            name: "ThemeIconsAndColorsLibrary",
+            targets: [
+                "ThemeColors",
+                "ThemeIcons"
+            ]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ThemeColors",
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .target(
+            name: "ThemeIcons",
+            resources: [
+                .process("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "ThemePackageTests",
+            dependencies: [
+                "ThemeColors",
+                "ThemeIcons"
+            ]
+        ),
+    ]
+)
---
+++ MyFirstPackageApp/MyFirstPackageApp/ThemePackage/Sources/ThemeIcons/ThemeIcons.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Daniel Ayala Jabon on 20/5/24.
+//
+
+import SwiftUI
+
+public enum ThemeIcons {
+    public static let bundle: Bundle = .module
+    public static let learn = Image("learn", bundle: ThemeIcons.bundle)
+    public static let people = Image("people", bundle: ThemeIcons.bundle)
+    public static let rocket = Image("rocket", bundle: ThemeIcons.bundle)
+    public static let settings = Image("settings", bundle: ThemeIcons.bundle)
+}
